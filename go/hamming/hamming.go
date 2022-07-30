9dd296431b498e0d4c2a7239c084ac723113582c
baroncurtin2 <knowledge3@gmail.com>
2022-07-29 20:08:27 -0400

    hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,20 @@
 package hamming
 
+import "errors"
+
+// Distance calculates the 'Hamming Distance' between two DNA sequences
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	// if lengths are different, return error
+	if len(a) != len(b) {
+		return -1, errors.New("DNA sequences must be the same length to compare")
+	}
+
+	count := 0
+	for i := range a {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+
+	return count, nil
 }
